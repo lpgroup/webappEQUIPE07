7eb1eb111c68de34fd283bbd6960c575ed5a1029
lpgroup <68185745+lpgroup@users.noreply.github.com>
2022-09-18 10:54:34 -0300

    Update webappEQUIPE07.py

--- webappEQUIPE07.py
+++ webappEQUIPE07.py
@@ -13,8 +13,8 @@ dataD = rD.content
 dfD = pd.read_csv(BytesIO(dataD), index_col=0)
 NregD = len(dfD)
 dfD.columns = ['email', 'equipe', 'nome', 'duvida', 'obs']
-selecao01D = dfD['equipe']=='Equipe 01'
-df01D = dfD[selecao01D]
+selecao07D = dfD['equipe']=='Equipe 07'
+df07D = dfD[selecao07D]
 
 #RESPOSTAS
 rR = requests.get('https://docs.google.com/spreadsheets/d/e/2PACX-1vQw6XD9vI_C4zvZ6W51vut_Ze_D_OSESuXiHh1IAXeBFXRRvyQ7kyFTLbGip1obadjbZHUmaAxvXmnz/pub?gid=1789345467&single=true&output=csv')
@@ -22,17 +22,17 @@ dataR = rR.content
 dfR = pd.read_csv(BytesIO(dataR), index_col=0)
 NregR = len(dfR)
 dfR.columns = ['email', 'equipe', 'nome', 'resposta', 'sugestao']
-selecao01R = dfR['equipe']=='Equipe 01'
-df01R = dfR[selecao01R]
+selecao07R = dfR['equipe']=='Equipe 07'
+df07R = dfR[selecao07R]
 
 #Cálculo do Número de Registros por EQUIPE
-NregDf01D = len(df01D)
-NregDf01R = len(df01R)
+NregDf07D = len(df07D)
+NregDf07R = len(df07R)
 
 
 image01 = Image.open('ImagemLateral.jpg')
 st.sidebar.image(image01, width=300, caption='Mack Week CCT 2022') 
-st.title("PAINEL - EQUIPE 01")
+st.title("PAINEL - EQUIPE 07")
 menu = ["Dúvidas",
         "Respostas",
         "Dúvidas e Respostas"]
@@ -41,15 +41,15 @@ st.sidebar.info("By: Prof. Massaki de O. Igarashi")
 
 if choice == "Dúvidas": 
     st.header("Relatório de DÚVIDAS")   
-    st.write('EQUIPE 01:')
+    st.write('EQUIPE 07:')
     st.warning('Dúvida(s) Enviada(s)')
-    st.code(df01D['duvida']) 
+    st.code(df07D['duvida']) 
            
 elif choice == "Respostas":       
     st.header("Relatório de RESPOSTAS")    
-    st.write('EQUIPE 01:')    
+    st.write('EQUIPE 07:')    
     st.info('Resposta do(a) TUTOR(A):')
-    st.code(df01R['resposta'])  
+    st.code(df07R['resposta'])  
 
                
 elif choice == "Dúvidas e Respostas":       
@@ -57,12 +57,12 @@ elif choice == "Dúvidas e Respostas":
     colDR1, colDR2 = st.columns((1,1))
     with colDR1:
         st.write("Nº TOTAL de Dúvidas (DESTA EQUIPE):")
-        st.warning(NregDf01D)
+        st.warning(NregDf07D)
     with colDR2:
         st.write("Nº TOTAL de dúvidas RESPONDIDAS:")
-        st.info(NregDf01R)
-    st.subheader('EQUIPE 01:')
+        st.info(NregDf07R)
+    st.subheader('EQUIPE 07:')
     st.warning('Dúvida(s) Enviada(s)')
-    st.code(df01D['duvida']) 
+    st.code(df07D['duvida']) 
     st.info('Resposta do(a) TUTOR(A):')
-    st.code(df01R['resposta'])  
+    st.code(df07R['resposta'])  
